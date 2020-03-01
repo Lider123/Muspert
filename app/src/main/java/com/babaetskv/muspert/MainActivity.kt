e4ea2890ae3b076075407afab3b7fdfb2cfdf9cc
babaetskv <k_babaec@mail.ru>
2020-03-01 15:55:19 +0500

    Initial commit

---
+++ app/src/main/java/com/babaetskv/muspert/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.babaetskv.muspert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
+}
