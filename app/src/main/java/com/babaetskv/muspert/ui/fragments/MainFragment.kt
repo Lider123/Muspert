c25bdf70146c0e4bf4f6292cbe32fc2d9e1029b6
lider123 <k_babaec@mail.ru>
2020-10-09 12:23:28 +0500

    Fixed bottom navigation

--- app/src/main/java/com/babaetskv/muspert/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/babaetskv/muspert/ui/fragments/MainFragment.kt
@@ -1,28 +1,34 @@
 package com.babaetskv.muspert.ui.fragments
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.fragment.app.Fragment
 import com.babaetskv.muspert.R
 import com.babaetskv.muspert.ui.base.BaseFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : BaseFragment() {
-    private lateinit var bottomNavController: NavController
+class MainFragment : BaseFragment(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override val layoutResId: Int
         get() = R.layout.fragment_main
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupNavigation()
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
     }
 
-    private fun setupNavigation() {
-        val fragmentContainer = requireView().findViewById<View>(R.id.bottomNavHostFragment)
-        bottomNavController = Navigation.findNavController(fragmentContainer!!)
-        NavigationUI.setupWithNavController(bottomNavigationView, bottomNavController)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val selectedFragment = when (item.itemId) {
+            R.id.catalog -> CatalogFragment()
+            R.id.feed -> FeedFragment()
+            R.id.profile -> ProfileFragment()
+            else -> Fragment()
+        }
+        childFragmentManager.beginTransaction()
+            .replace(R.id.container, selectedFragment)
+            .commit()
+        return true
     }
 }
